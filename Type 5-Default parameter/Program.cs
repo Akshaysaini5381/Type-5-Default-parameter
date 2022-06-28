5b55994c5f7abdf77884f9bc1407a29d330966a4
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:30:31 +0530

    Add project files.

---
+++ Type 5-Default parameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Type_5_Default_parameter
+{
+    class Program
+    {
+       static void show(int num,int num1,char choice = '+')
+        {
+            if (choice=='+')
+            {
+                Console.WriteLine("Sum :"+(num+num1));
+            }
+            else if (choice=='-')
+            {
+                Console.WriteLine( num - num1);
+            }
+            else
+            {
+                Console.WriteLine("Not found :");
+            }
+        }
+        static void Main(string[] args)
+        {
+            show(10, 20);
+        }
+    }
+}
